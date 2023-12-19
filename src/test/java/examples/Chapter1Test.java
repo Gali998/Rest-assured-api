6f3d12f5321e357ee3064a5cc17a86b9cd2dc7f1
Gali998 <galimannage6.gm@gmail.com>
2023-12-19 20:57:09 +0530

    Request details updated

--- src/test/java/examples/Chapter1Test.java
+++ src/test/java/examples/Chapter1Test.java
@@ -5,6 +5,23 @@ import org.junit.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+/**
+ * Retrieve country details - http://zippopotam.us/us/90210
+ * {
+ *     "post code": "90210",
+ *     "country": "United States",
+ *     "country abbreviation": "US",
+ *     "places": [
+ *         {
+ *             "place name": "Beverly Hills",
+ *             "longitude": "-118.4065",
+ *             "state": "California",
+ *             "state abbreviation": "CA",
+ *             "latitude": "34.0901"
+ *         }
+ *     ]
+ * }
+ */
 public class Chapter1Test {
 
     @Test
