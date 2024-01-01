80c5a28cc336f527c5e54b9d7157bba4a2d2f260
Gali998 <galimannage6.gm@gmail.com>
2024-01-01 19:03:59 +0530

    Check content type

--- src/test/java/examples/Chapter2Test.java
+++ src/test/java/examples/Chapter2Test.java
@@ -1,5 +1,6 @@
 package examples;
 
+import io.restassured.http.ContentType;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.*;
@@ -7,12 +8,22 @@ import static io.restassured.RestAssured.*;
 public class Chapter2Test {
 
     @Test
-    public void checkStatusCode(){
+    public void checkStatusCode() {
         given().
                 when().
                 get("http://zippopotam.us/us/90210").
                 then().
                 assertThat().
                 statusCode(200);
+    }
+
+    @Test
+    public void checkContentType() {
+        given().
+                when().
+                get("http://zippopotam.us/us/90210").
+                then().
+                assertThat().
+                contentType(ContentType.JSON);
     }
 }
